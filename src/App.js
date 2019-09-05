81a86ca4798cd2ac71a5b1e8364b7fdd4860474d
laukey <206296435@qq.com>
2019-09-05 16:10:13 +0800

    调整模版

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.less';
+import { Button } from 'antd-mobile'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{fontSize: '20px'}}>
       Hello React
+      <Button type="primary">Start</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'lib-flexible';
+import initReactFastclick from 'react-fastclick';
+// import 'antd-mobile/dist/antd-mobile.less'
+
+initReactFastclick();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
